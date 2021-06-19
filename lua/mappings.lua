610e4084fc7b3589349820c744d1f078e3943485
Thiago Pontes <github@thiago.me>
2021-06-19 14:45:42 -0400

    add nvim tree mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,3 +7,6 @@ vim.api.nvim_set_keymap("n", "<Leader>fn", [[<Cmd> DashboardNewFile<CR>]], {nore
 
 -- eval current lua file
 vim.api.nvim_set_keymap("n", "<Leader>rr", [[<Cmd> luafile %<CR>]], {noremap = true, silent = true})
+
+-- open/close nvim tree
+vim.api.nvim_set_keymap("n", "<Leader>k", [[<Cmd> NvimTreeToggle<CR>]], {noremap = true, silent = true})
