620dbc908f362f03c8438986a9b5efd07da71dce
Thiago Pontes <github@thiago.me>
2021-06-19 14:43:23 -0400

    load dashboard file, move some mappings to mappings file

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
 -- when in terminal mode(:term), map ESC to exit instead of the default c-\ c-n
 vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], {noremap = true, silent = true})
+
+-- create new file
+vim.api.nvim_set_keymap("n", "<Leader>fn", [[<Cmd> DashboardNewFile<CR>]], {noremap = true, silent = true})
+
+-- eval current lua file
+vim.api.nvim_set_keymap("n", "<Leader>rr", [[<Cmd> luafile %<CR>]], {noremap = true, silent = true})
