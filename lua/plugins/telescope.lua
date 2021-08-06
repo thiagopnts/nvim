e194eedac4e5d4b588b8b3303433f3c9c3e976e4
Thiago Pontes <github@thiago.me>
2021-08-06 12:17:34 -0400

    update telescope options to latest version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,18 +10,16 @@ require("telescope").setup {
       "--column",
       "--smart-case"
     },
-    prompt_position = "bottom",
-    prompt_prefix = " ",
-    selection_caret = " ",
+    prompt_prefix = "> ",
+    selection_caret = "> ",
     entry_prefix = "  ",
     initial_mode = "insert",
     selection_strategy = "reset",
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
-    layout_defaults = {
+    layout_config = {
       horizontal = {
-        mirror = false,
-        preview_width = 0.5
+        mirror = false
       },
       vertical = {
         mirror = false
@@ -30,28 +28,18 @@ require("telescope").setup {
     file_sorter = require "telescope.sorters".get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-    shorten_path = true,
     winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
     border = {},
     borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
     color_devicons = true,
     use_less = true,
+    path_display = {},
     set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
     file_previewer = require "telescope.previewers".vim_buffer_cat.new,
     grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
     qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
     -- Developer configurations: Not meant for general override
     buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
-  },
-  extensions = {
-    media_files = {
-      filetypes = {"png", "webp", "jpg", "jpeg"},
-      find_cmd = "rg" -- find command (defaults to `fd`)
-    }
   }
 }
 
