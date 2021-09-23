468441afe4b150944799f4a33a9244764ed87038
Thiago Pontes <github@thiago.me>
2021-09-23 10:37:59 -0400

    Use smaller circle for signs

--- init.lua
+++ init.lua
@@ -292,10 +292,10 @@ cmp.setup {
 
 local saga = require("lspsaga")
 saga.init_lsp_saga {
-  error_sign = '⬤',
-  warn_sign = '⬤',
-  hint_sign = '⬤',
-  infor_sign = '⬤',
+  error_sign = '⚫',
+  warn_sign = '⚫',
+  hint_sign = '⚫',
+  infor_sign = '⚫',
   rename_prompt_prefix = '',
 }
 
